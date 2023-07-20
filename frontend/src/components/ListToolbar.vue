924bf2356d531147a46047ca8570941d11dd8b5a
Lili <lauvef@uni-mainz.de>
2023-07-20 14:21:04 +0200

    delete all items button

--- frontend/src/components/ListToolbar.vue
+++ frontend/src/components/ListToolbar.vue
@@ -9,6 +9,7 @@
                 v-model="itemName"
         ></v-text-field>
         <v-btn v-if="creating" @click="addItem" text>{{createButtonText}}</v-btn>
+        <v-btn @click="deleteAllItems" :color="emptyButtonColor" text>{{emptyButtonText}}</v-btn>
         <v-btn @click="deleteList" :color="deleteButtonColor" text>{{deleteButtonText}}</v-btn>
     </div>
 </template>
@@ -22,6 +23,7 @@
         data: () => ({
             creating: false,
             deleting: false,
+            emptying: false,
             itemName: ''
         }),
         computed: {
@@ -31,8 +33,14 @@
             deleteButtonText: function() {
                 return this.deleting? 'Sicher?' : 'Liste löschen';
             },
+            emptyButtonText: function() {
+                return this.emptying? 'Sicher?' : 'Liste leeren';
+            },
             deleteButtonColor: function() {
                 return this.deleting? 'error' : '';
+            },
+            emptyButtonColor: function() {
+                return this.emptying? 'error' : '';
             }
         },
         methods: {
@@ -57,6 +65,24 @@
                     const that = this;
                     const timer = window.setTimeout(function() {
                         that.deleting = false;
+                    }, 3000);
+                }
+            },
+            deleteAllItems: async function() {
+                if (this.emptying) {
+                    let items = this.list.items;
+                    console.log(items)
+                    for (const item of items) {
+                        items = await _items.deleteItem(item.id);
+                        console.log(items)
+                    }
+                    this.$emit('setItems', items);
+                    this.emptying = false;
+                } else {
+                    this.emptying = true;
+                    const that = this;
+                    const timer = window.setTimeout(function() {
+                        that.emptying = false;
                     }, 3000);
                 }
             }
